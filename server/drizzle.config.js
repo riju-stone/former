329b6cf23f3f70dbb66303bcf5866afbf0be6e5b
Arighna Chakraborty <riju23chakra@gmail.com>
2025-12-27 23:12:15 +0530

    fix: server build errors

---
+++ server/drizzle.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const drizzle_kit_1 = require("drizzle-kit");
+exports.default = (0, drizzle_kit_1.defineConfig)({
+    out: "./drizzle",
+    schema: "./src/db/schema",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.POSTGRES_URL,
+    },
+});
