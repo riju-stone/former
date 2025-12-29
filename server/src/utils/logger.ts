61e33561b52bd7fab9d55e70a3fa56bb91383017
Arighna Chakraborty <riju23chakra@gmail.com>
2025-12-29 18:32:37 +0530

    fix: server prod logger errors

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -3,21 +3,28 @@ import pino from "pino";
 
 const LOG_DATE = new Date().toISOString().split("T")[0];
 
-const transport = pino.transport({
-  targets: [
-    {
-      target: "pino-pretty",
-      options: { colorize: true, ignore: "hostname" },
+const targets: any[] = [];
+
+if (env.NODE_ENV === "dev") {
+  targets.push({
+    target: "pino-pretty",
+    options: { colorize: true, ignore: "hostname" },
+  });
+  targets.push({
+    target: "pino/file",
+    options: {
+      destination: `${env.LOG_DIR}/${LOG_DATE}.log`,
+      mkdir: true,
     },
-    {
-      target: "pino/file",
-      options: {
-        destination: `${env.LOG_DIR}/${LOG_DATE}.log`,
-        mkdir: true,
-      },
-    },
-  ],
-});
+  });
+}
+
+const transport =
+  targets.length > 0
+    ? pino.transport({
+      targets,
+    })
+    : undefined;
 
 const customLogger = pino(
   {
